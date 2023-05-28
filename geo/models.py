ed2ffd5d0b1086e9f004d05089f6fe6c483bb1e2
Danylo <danildacenko@gmail.com>
2023-05-28 21:08:36 +0300

    allowed to description filed be a null, blank

--- geo/models.py
+++ geo/models.py
@@ -5,5 +5,5 @@ from django.contrib.gis.db import models
 
 class Place(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     geom = models.PointField()
