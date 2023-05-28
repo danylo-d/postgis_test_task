4eb6bc86715ee9c8af772e2b8655fd800336d291
Danylo <danildacenko@gmail.com>
2023-05-28 18:41:07 +0300

    Created viewset for CRUD Place model

--- geo/views.py
+++ geo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from geo.models import Place
+from geo.serializers import PlaceSerializer
+
 
 # Create your views here.
+class PlaceViewSet(viewsets.ModelViewSet):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
