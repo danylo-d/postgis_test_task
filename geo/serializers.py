19a44579989ce001f53f3794f7ff37d1511bb8b6
Danylo <danildacenko@gmail.com>
2023-05-28 21:09:37 +0300

    Added validation to serializer and created List serializer for Place model

--- geo/serializers.py
+++ geo/serializers.py
@@ -8,16 +8,17 @@ class PointFieldSerializer(serializers.Field):
     def to_representation(self, value):
         if isinstance(value, Point):
             return {"type": "Point", "coordinates": [value.x, value.y]}
-        return value
+        return super().to_representation(value)
 
     def to_internal_value(self, data):
         if isinstance(data, str):
             try:
                 x, y = map(float, data.split(","))
-                return Point(x, y)
-            except (ValueError, TypeError):
-                pass
-        return data
+                return Point(x, y, srid=4326)
+            except ValueError:
+                raise serializers.ValidationError(
+                    "Invalid geom value. Must be in the format of 'x, y'."
+                )
 
 
 class PlaceSerializer(serializers.ModelSerializer):
@@ -25,4 +26,18 @@ class PlaceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Place
-        fields = ("id", "name", "description", "geom")
+        fields = ["id", "name", "description", "geom"]
+
+
+class PlaceListSerializer(PlaceSerializer):
+    geom = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Place
+        fields = ["id", "name", "geom"]
+
+    @staticmethod
+    def get_geom(obj):
+        if isinstance(obj.geom, Point):
+            return [obj.geom.x, obj.geom.y]
+        return None
