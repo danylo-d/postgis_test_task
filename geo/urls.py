940cd0ce68be31bf6b1c14ebdaa64075c4436ac5
Danylo <danildacenko@gmail.com>
2023-05-28 18:40:32 +0300

    added router to places endpoint

--- geo/urls.py
+++ geo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from geo.views import PlaceViewSet
+
 router = routers.DefaultRouter()
+router.register("places", PlaceViewSet)
 urlpatterns = [path("", include(router.urls))]
 
 app_name = "geo"
