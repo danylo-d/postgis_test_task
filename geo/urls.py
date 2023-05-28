19b4f9927885e74f646c6974343f45a2e1f35ca4
Danylo <danildacenko@gmail.com>
2023-05-28 21:10:27 +0300

    added endpoit to find nearest place by coords

--- geo/urls.py
+++ geo/urls.py
@@ -5,6 +5,13 @@ from geo.views import PlaceViewSet
 
 router = routers.DefaultRouter()
 router.register("places", PlaceViewSet)
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "nearest_place/",
+        PlaceViewSet.as_view({"get": "get_nearest_place"}),
+        name="place-nearest",
+    ),
+]
 
 app_name = "geo"
