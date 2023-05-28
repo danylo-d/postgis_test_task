eece969db4f4528de1e4f5416799bdf2b58db694
Danylo <danildacenko@gmail.com>
2023-05-28 17:31:43 +0300

    creating model Place, and migrations

--- geo/models.py
+++ geo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.gis.db import models
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    geom = models.PointField()
--- postgis_test_task/settings.py
+++ postgis_test_task/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "geo"
+    "django.contrib.gis",
+    "geo",
 ]
 
 MIDDLEWARE = [
