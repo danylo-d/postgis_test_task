1eda30cfc4aa1e07345a82d18536f8f363f93a06
Danylo <danildacenko@gmail.com>
2023-05-29 13:29:02 +0300

    added to fields for creation geo-position, longitude and latitude

--- geo/serializers.py
+++ geo/serializers.py
@@ -10,23 +10,30 @@ class PointFieldSerializer(serializers.Field):
             return {"type": "Point", "coordinates": [value.x, value.y]}
         return super().to_representation(value)
 
-    def to_internal_value(self, data):
-        if isinstance(data, str):
-            try:
-                x, y = map(float, data.split(","))
-                return Point(x, y, srid=4326)
-            except ValueError:
-                raise serializers.ValidationError(
-                    "Invalid geom value. Must be in the format of 'x, y'."
-                )
-
 
 class PlaceSerializer(serializers.ModelSerializer):
-    geom = PointFieldSerializer()
+    longitude = serializers.FloatField(write_only=True)
+    latitude = serializers.FloatField(write_only=True)
+    geom = PointFieldSerializer(read_only=True)
 
     class Meta:
         model = Place
-        fields = ["id", "name", "description", "geom"]
+        fields = ["id", "name", "description", "longitude", "latitude", "geom"]
+
+    def create(self, validated_data):
+        longitude = validated_data.pop("longitude", None)
+        latitude = validated_data.pop("latitude", None)
+        if longitude is not None and latitude is not None:
+            geom = Point(longitude, latitude, srid=4326)
+            validated_data["geom"] = geom
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        longitude = validated_data.pop("longitude", None)
+        latitude = validated_data.pop("latitude", None)
+        if longitude is not None and latitude is not None:
+            instance.geom = Point(longitude, latitude, srid=4326)
+        return super().update(instance, validated_data)
 
 
 class PlaceListSerializer(PlaceSerializer):
@@ -34,10 +41,10 @@ class PlaceListSerializer(PlaceSerializer):
 
     class Meta:
         model = Place
-        fields = ["id", "name", "geom"]
+        fields = ["id", "name", "longitude", "latitude", "geom"]
 
     @staticmethod
     def get_geom(obj):
         if isinstance(obj.geom, Point):
-            return [obj.geom.x, obj.geom.y]
+            return {"longitude": obj.geom.x, "latitude": obj.geom.y}
         return None
