952717ce603115cb78e994345ca7e03964a380fd
Danylo <danildacenko@gmail.com>
2023-05-30 00:44:17 +0300

    changed tests for new features

--- geo/tests/test_serializers.py
+++ geo/tests/test_serializers.py
@@ -1,23 +1,42 @@
 from django.test import TestCase
 from django.contrib.gis.geos import Point
-
 from geo.models import Place
-from geo.serializers import PointFieldSerializer, PlaceListSerializer
+from geo.serializers import PlaceSerializer, PlaceListSerializer
 
 
-class PointFieldSerializerTestCase(TestCase):
-    def test_to_representation(self):
-        serializer = PointFieldSerializer()
-        point = Point(1.23, 4.56, srid=4326)
-        representation = serializer.to_representation(point)
-        expected_representation = {"type": "Point", "coordinates": [1.23, 4.56]}
-        self.assertEqual(representation, expected_representation)
+class PlaceSerializerTest(TestCase):
+    def test_create(self):
+        serializer = PlaceSerializer()
+        validated_data = {
+            'name': 'Test Place',
+            'description': 'Test Description',
+            'longitude': 1.23,
+            'latitude': 4.56
+        }
+        place = serializer.create(validated_data)
+        self.assertIsInstance(place, Place)
+        self.assertEqual(place.name, 'Test Place')
+        self.assertEqual(place.description, 'Test Description')
+        self.assertEqual(place.geom, Point(1.23, 4.56, srid=4326))
 
+    def test_update(self):
+        place = Place.objects.create(name='Test Place', description='Old Description', geom=Point(0, 0, srid=4326))
+        serializer = PlaceSerializer(instance=place)
+        validated_data = {
+            'name': 'Updated Place',
+            'description': 'New Description',
+            'longitude': 7.89,
+            'latitude': 0.12
+        }
+        updated_place = serializer.update(place, validated_data)
+        self.assertEqual(updated_place.name, 'Updated Place')
+        self.assertEqual(updated_place.description, 'New Description')
+        self.assertEqual(updated_place.geom, Point(7.89, 0.12, srid=4326))
+
+
+class PlaceListSerializerTest(TestCase):
     def test_get_geom(self):
-        serializer = PlaceListSerializer()
-        place_instance = Place.objects.create(
-            name="Test Place", geom=Point(1.23, 4.56, srid=4326)
-        )
-        geom = serializer.get_geom(place_instance)
-        expected_geom = {"longitude": 1.23, "latitude": 4.56}
-        self.assertEqual(geom, expected_geom)
+        place = Place(name="Test Place", geom=Point(1.23, 4.56, srid=4326))
+        serializer = PlaceListSerializer(instance=place)
+        geom = serializer.get_geom(place)
+        self.assertEqual(geom, {"longitude": 1.23, "latitude": 4.56})
--- geo/tests/test_views.py
+++ geo/tests/test_views.py
@@ -9,50 +9,29 @@ from geo.views import PlaceViewSet
 class PlaceViewSetTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-        self.view = PlaceViewSet.as_view({"get": "get_nearest_place"})
+        self.view = PlaceViewSet.as_view({"get": "nearest_place"})
         self.place = Place.objects.create(
             name="Place 1", description="Description 1", geom=Point(0, 0, srid=4326)
         )
 
-    def test_get_nearest_place_success(self):
-        request = self.factory.get(
-            "/places/nearest-place", {"latitude": 1, "longitude": 1}
-        )
+    def test_nearest_place_success(self):
+        request = self.factory.get("/nearest-place", {"latitude": 1, "longitude": 1})
         response = self.view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["name"], self.place.name)
 
-    def test_get_nearest_place_wrong_parameters(self):
-        request = self.factory.get("/places/nearest-place/?latitude=12.34&longitude=test+")
+    def test_nearest_place_wrong_parameters(self):
+        request = self.factory.get("/nearest-place/?latitude=12.34&longitude=test+")
         response = self.view(request)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
             response.data["error"], "Invalid longitude or latitude values provided."
         )
 
-    def test_get_nearest_place_missing_parameters(self):
-        request = self.factory.get("/places/nearest-place")
+    def test_nearest_place_missing_parameters(self):
+        request = self.factory.get("/nearest-place")
         response = self.view(request)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
             response.data["error"], "Longitude and latitude parameters are required."
         )
-
-    def test_nearest_place_excludes_current_place(self):
-        other_place = Place.objects.create(
-            name="Place 2", description="Description 2", geom=Point(2, 2, srid=4326)
-        )
-        request = self.factory.get("/places/{}/nearest_place".format(self.place.id))
-        response = PlaceViewSet.as_view({"get": "nearest_place"})(
-            request, pk=self.place.id
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["name"], other_place.name)
-
-    def test_nearest_place_handles_invalid_place_id(self):
-        invalid_id = 9999
-        request = self.factory.get("/places/{}/nearest_place".format(invalid_id))
-        response = PlaceViewSet.as_view({"get": "nearest_place"})(
-            request, pk=invalid_id
-        )
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
