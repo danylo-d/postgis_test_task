34511b8c27fe8352fdb0fee533495753b6b42e22
Danylo <danildacenko@gmail.com>
2023-05-29 15:35:40 +0300

    migrations

--- geo/models.py
+++ geo/models.py
@@ -6,5 +6,10 @@ class Place(models.Model):
     description = models.TextField(blank=True, null=True)
     geom = models.PointField()
 
+    class Meta:
+        indexes = [
+            models.Index(fields=["geom"]),
+        ]
+
     def __str__(self):
         return f"{self.name} ({self.geom.x}, {self.geom.y})"
